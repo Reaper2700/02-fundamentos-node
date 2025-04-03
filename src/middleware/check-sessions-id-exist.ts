ba215ffe563dc866fd52adf55da1f9192e66e0a7
Reaper <rafaell_bb@hotmail.com>
2025-04-02 23:59:51 -0300

    Admin global

--- src/middleware/check-sessions-id-exist.ts
+++ src/middleware/check-sessions-id-exist.ts
@@ -14,9 +14,9 @@ export async function checkSessionIdExist(
 }
 
 export async function AdminCheck(request: FastifyRequest, reply: FastifyReply) {
-  const { adminId } = request.cookies
+  const { sessionId } = request.cookies
 
-  if (adminId !== 'Reaper270') {
+  if (sessionId !== '5003bfca-7adb-4bd3-a1e8-b2a8f0f2980e') {
     return reply.status(403).send({ error: 'Acesso negado' })
   }
 }
